1ead6d2ee2be6f0472687e15a64ff65fd8feaafe
Oleg Ten <tennet0505@gmail.com>
2024-11-07 14:54:07 +0100

    Initial Commit

---
+++ ChatWithAI/ChatWithAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatWithAIApp.swift
+//  ChatWithAI
+//
+//  Created by Oleg Ten on 07/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChatWithAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatWithAITests/ChatWithAITests.swift
@@ -0,0 +1,17 @@
+//
+//  ChatWithAITests.swift
+//  ChatWithAITests
+//
+//  Created by Oleg Ten on 07/11/2024.
+//
+
+import Testing
+@testable import ChatWithAI
+
+struct ChatWithAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
